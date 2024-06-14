f98475eec0ca94e50f94fb427f9a4b6220099116
Misael Flores <63472060+misadevflores@users.noreply.github.com>
2024-06-14 15:18:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY! ROLANDO'  ],
+			strings: [ 'HAPPY', 'BIRTHDAY! SILVIA PILAR'  ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,

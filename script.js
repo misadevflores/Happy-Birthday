677fc5c9b47a925a212d69a3a7d5a37bab86443a
Misael Flores <63472060+misadevflores@users.noreply.github.com>
2025-02-05 21:18:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'KEVIN BRAYAN GOMEZ' ,'Cada bug que resuelves te hace más fuerte'],
+			strings: [ 'HAPPY', 'BIRTHDAY!', 'KEVIN BRAYAN GOMEZ' ,'Cada bug que resuelves','te hace más fuerte'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
